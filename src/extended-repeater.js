99c0d2bc6178d8b11c304c4dfee0db579d7e12e2
Anna <61065956+AnnaIgnatova@users.noreply.github.com>
2021-03-15 21:31:59 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,13 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  repTimes = 0, sep = '+', add = '', addRepTime = 0, addSep = '|', strSolution = '';
+  repTimes = 1, sep = '+', add = '', addRepTime = 1, addSep = '|', strSolution = '';
   for (key in options) {
     if (key == 'repeatTimes') repTimes = options[key];
     if (key == 'separator') sep = options[key];
     if (key == 'addition') add = options[key] + '';
     if (key == 'additionRepeatTimes') addRepTime = options[key];
     if (key == 'additionSeparator') addSep = options[key];
+
   }
   if (repTimes === undefined || addRepTime === undefined || repTimes === 0) return 'TESTstrADD!';
   for (let i = 0; i < repTimes; i++) {
